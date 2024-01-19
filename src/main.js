8dff28e317db62328067c2d3dc7430a425733aff
nilesh-mule <nileshmule1448@gmail.com>
2024-01-19 16:19:20 +0530

    first code

--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// main.js
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import Home from './components/HomeComp.vue';
+import About from './components/aboutcomp.vue';
+import blog from './components/blogcomp.vue';
+import contact from './components/contactcomp.vue';
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+  { path: '/blog', component: blog },
+  { path: '/contact', component: contact },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+new Vue({
+  render: (h) => h(App),
+  router,
+}).$mount('#app');
